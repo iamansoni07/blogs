ec567ffc645d75d6a194027a20f09a297563ecc6
iamansoni07 <aman.soni0713@gmail.com>
2024-10-22 23:23:26 +0530

    commits

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -4,6 +4,9 @@ import logo from "./imgs/logo.png";
 const Navbar = () => {
   return (
     <nav className="navbar">
+        <Link to="/" className="flex-none w-10">
+            <img src={logo} className="w-full" />
+        </Link>
 
     </nav>
   );
