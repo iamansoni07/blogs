aa88bd65de57eb3ae23f8f12cdd48f91313585c3
iamansoni07 <aman.soni0713@gmail.com>
2024-10-22 23:22:57 +0530

    commits

---
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import logo from "./imgs/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+
+    </nav>
+  );
+};
+
+export default Navbar;
